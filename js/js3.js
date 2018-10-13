81fb5fe78870654769318c79e5dcfe5daf3fa45f
xxxWALDxxx <noki.asd4321@gmail.com>
2018-10-13 17:12:29 +0300

    13.10.2018

--- js/js3.js
+++ js/js3.js
@@ -12,3 +12,8 @@ $(document).ready(function(){
     menu.toggleClass('menu-active');
   });
 });
+$('.slider').slick({
+  arrows:true,
+  prevArrow: '<button class="l-arrow" type="button"><img src="img/left.png"></button>',
+  nextArrow: '<button class="r-arrow" type="button"><img src="img/right.png"></button>'
+});
