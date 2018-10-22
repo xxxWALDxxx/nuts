d25a142e8e58df2c15e857c1c1890fa16a4566ab
xxxWALDxxx <noki.asd4321@gmail.com>
2018-10-22 23:19:27 +0300

    22.10.2019

---
+++ js/contacts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [54.752656, 56.002053],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/maps.png',
+            // Размеры метки.
+            iconImageSize: [64, 64],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-32, -64]
+        });
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+});
+  var link=$('.menu-link');
+  var link_active=$('.menu-link-active');
+  var menu=$('.menu');
+  var nav_link=$('.menu a');
+  link.click(function(){
+    link.toggleClass('menu-link-active');
+    menu.toggleClass('menu-active');
+  });
+  nav_link.click(function(){
+    link.toggleClass('menu-link-active');
+    menu.toggleClass('menu-active');
+  });
+});
