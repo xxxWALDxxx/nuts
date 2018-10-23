9fdcbd5d9935888d71d9cdbf5a75499f3599f2b6
xxxWALDxxx <noki.asd4321@gmail.com>
2018-10-23 21:53:06 +0300

    Почти готовый сайт

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-  ymaps.ready(function () {
+ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
             center: [54.752656, 56.002053],
             zoom: 17
@@ -28,10 +27,39 @@ $(document).ready(function(){
             iconImageOffset: [-32, -64]
         });
     myMap.geoObjects
-        .add(myPlacemark)
-        .add(myPlacemarkWithContent);
+        .add(myPlacemark);
+    var myMap2 = new ymaps.Map('map2', {
+            center: [51.752656, 56.002053],
+            zoom: 5
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/maps.png',
+            // Размеры метки.
+            iconImageSize: [64, 64],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-32, -64]
+        });
+    myMap2.geoObjects
+        .add(myPlacemark);
 });
-  var link=$('.menu-link');
+$(document).ready(function(){
+ var link=$('.menu-link');
   var link_active=$('.menu-link-active');
   var menu=$('.menu');
   var nav_link=$('.menu a');
